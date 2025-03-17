74fa8a8e9e90434e9b020391d74a0a05db96a8cb
matthewxu <128595283+matthxu@users.noreply.github.com>
2025-03-17 22:28:39 +1100

    Added week 2 exercises

---
+++ week2/verification - MatthewXu105645875/verification/task2.1/counter/Program.cs
@@ -0,0 +1,46 @@
+﻿public class Clock
+{
+    private int _MinuteCounter;
+    private int _SecondCounter;
+
+    public int MinuteCounter
+    {
+        get { return _MinuteCounter; }
+        set { _MinuteCounter = value; }
+    }
+    
+    public int SecondCounter
+    {
+        get { return _SecondCounter; }
+        set { _SecondCounter = value; }
+    }
+
+
+    public Clock()
+    {
+        _SecondCounter = 0;
+        _MinuteCounter = 0;
+    }
+
+    public void Tick(){
+        _SecondCounter += 1;
+        if (_SecondCounter == 60) {
+            _SecondCounter = 0;
+            _MinuteCounter += 1;
+        }
+    }
+    public void Display() {
+        Console.WriteLine($"{MinuteCounter}:{SecondCounter}");
+    }
+}
+
+public class Program
+{
+    static void Main () {
+        Clock clock = new Clock();
+        for (int i=0; i<=100; i++) {
+            clock.Tick();
+            clock.Display();
+        }
+    }
+}
