1070f002f710de10124bf36a58aa8eb305dd82a3
Matthew <128595283+matthxu@users.noreply.github.com>
2025-03-18 12:59:16 +1100

    Added task 3.1

---
+++ week3/task3.1/ObjectUnitTests/Program.cs
@@ -0,0 +1,44 @@
+﻿public class IdentifiableObject 
+{
+    private List<string> _identifiers;
+    // Constructor
+    public IdentifiableObject(List<string> identifier) {
+        _identifiers = identifier;
+    }
+    // Methods
+    public bool AreYou(string stringID) {
+        if (_identifiers.Contains(stringID)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+     // read only
+    public string FirstId
+    {
+        get { return _identifiers.Count > 0 ? _identifiers[0] : ""; }
+        // alternatively...
+        // get
+        // {
+        //     if (_identifiers.Count > 0) {
+        //         return _identifiers[0];
+        //     } else {
+        //         return ""; // return empty string if _identifiers empty
+        //     }
+        // }
+    }
+    public void AddIdentifier(string identifier) {
+        _identifiers.Add(identifier.ToLower());
+    }
+    public void PrivilegeEscalation(string Id) {
+        if (Id == "5875") {
+            _identifiers[0] = Id;
+        }
+    }
+    public void RemoveIdentifier(string identifier) {
+        identifier.ToLower();
+        if (_identifiers.Contains(identifier)) {
+            _identifiers.Remove(identifier);
+        }
+    }
+}
---
+++ week3/task3.1/appTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿namespace appTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestAreYou()
+    {
+        if (name) {
+
+        }
+        Assert.That();
+    }
+    
+    [Test]
+    public void TestNotAreYou(result)
+    {
+        Assert.That(result, Is.True, $"{result} is not the same.");
+    }
+    
+}
+
+
+
+    [Test]
+    public void CheckStartCount()
+    {
+        Counter countNum = new Counter("TestCounter"); // create new instance of Counter
+        Assert.That(countNum.Ticks, Is.EqualTo(0)); // check starting count is 0.
+    }
+
+    [Test]
+    public void CheckIncrements() 
+    {
+        Counter testNum = new Counter("IncrementTester");
+        testNum.Increment(); // call increment method
+        Assert.That(testNum.Ticks, Is.EqualTo(5)); // if increment is 5
+    }
+
+    [Test]
+    public void CheckResetValue() 
+    {
+        Counter resetNum = new Counter("TestResetter");
+        resetNum.ResetByDefault(); // call on reset method
+        Assert.That(resetNum.Ticks, Is.EqualTo(214748365875)); // check reset method produces expected long
+    }
