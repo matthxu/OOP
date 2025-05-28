63feff2500bbc50eeaae9e28ef417df4fdb97acd
matthewxu <128595283+matthxu@users.noreply.github.com>
2025-05-28 20:25:05 +1000

    Added week12 clock in C#

---
+++ week12/ClockAppTemplate/ClockAppTemplate/Clock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace ClockApp
+{
+    public class Clock
+    {
+        private Counter _hour;
+        private Counter _min;
+        private Counter _sec;
+
+        public Clock()
+        {
+            _hour = new Counter("Hours");
+            _min = new Counter("Minutes");
+            _sec = new Counter("Seconds");
+        }
+
+        public string GetTime()
+        {
+            string h = _hour.Ticks.ToString("D2");
+            string m = _min.Ticks.ToString("D2");
+            string s = _sec.Ticks.ToString("D2");
+            string time = h + ":" + m + ":" + s;
+
+            return time;
+        }
+
+        public void Restart()
+        {
+            _hour.Reset();
+            _min.Reset();
+            _sec.Reset();
+        }
+
+        public void Tick()
+        {
+            _sec.Increment();
+
+            if (_sec.Ticks > 60)
+            {
+                _min.Increment();
+                _sec.Reset();
+
+                if (_min.Ticks > 60)
+                {
+                    _hour.Increment();
+                    _min.Reset();
+                }
+                if (_hour.Ticks > 12)
+                {
+                    _hour.Reset();
+                }
+            }
+
+
+        }
+    }
+}
---
+++ week12/ClockAppTemplate/ClockAppTemplate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ClockApp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Clock MyClock = new Clock();
+            for (int i = 0; i < 10000; i++)
+            {
+                MyClock.Tick();
+            }
+
+            //Get the current process
+            System.Diagnostics.Process proc = System.Diagnostics.Process.GetCurrentProcess();
+            Console.WriteLine("Current process: {0}", proc.ToString());
+            //Display the total physical memory size allocated for the current process
+            Console.WriteLine("Physical memory usage: {0} bytes", proc.WorkingSet64);
+            // Display peak memory statistics for the process.
+            Console.WriteLine("Peak physical memory usage {0} bytes", proc.PeakWorkingSet64);
+            //extract memory
+
+            //extract the timelapse
+            Console.WriteLine(MyClock.GetTime());
+            MyClock.Restart();
+            Console.WriteLine(MyClock.GetTime());
+            stopwatch.Stop();
+        }
+    }
+}
